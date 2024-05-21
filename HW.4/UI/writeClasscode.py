dbc0c1ae22604535760ed9b6b9282bc7fc52e4d4
kangmunjeong <kmj7232@gmail.com>
2024-05-21 23:00:03 +0900

    클래스명 변경, 파일명 변경

--- HW.4/UI/writeClasscode.py
+++ HW.4/UI/writeClasscode.py
@@ -3,7 +3,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_Dialog(object):
+class WriteClassCode(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
         Dialog.resize(400, 136)
@@ -40,7 +40,7 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     Dialog = QtWidgets.QDialog()
-    ui = Ui_Dialog()
+    ui = WriteClassCode()
     ui.setupUi(Dialog)
     Dialog.show()
     sys.exit(app.exec_())
